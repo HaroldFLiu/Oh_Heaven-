91aae87a2ddbf59e64ae9e734529d87ef8a73068
laviyre <54475683+laviyre@users.noreply.github.com>
2022-05-26 14:35:07 +1000

    Add player class

---
+++ src/oh_heaven/game/Player.java
@@ -0,0 +1,51 @@
+package oh_heaven.game;
+import ch.aplu.jcardgame.*;
+
+public abstract class Player
+{
+    private static final int madeBidBonus = 10;
+    private static int playerCount = 0;
+
+    private Card selected;
+
+    private Hand hand;
+    private int playerScore;
+    private int tricksWon;
+    private int bid;
+    private int playerNumber;
+
+    public Player()
+    {
+        playerNumber = playerCount++;
+    }
+
+    public void startRound(Deck deck)
+    {
+        tricksWon = 0;
+        hand = new Hand(deck);
+    }
+
+    public void updateScore()
+    {
+        playerScore += tricksWon;
+        if (tricksWon == bid)
+            playerScore += madeBidBonus;
+    }
+
+    public abstract int makeBid(boolean lastBid, int previousBids, int nbStartCards);
+
+    public abstract void selectCard();
+
+    // TODO CHANGE TO TRICK CLASS
+    public abstract void playCard();
+
+    public void winTrick()
+    {
+        tricksWon++;
+    }
+
+    public Hand getHand()
+    {
+        return hand;
+    }
+}
