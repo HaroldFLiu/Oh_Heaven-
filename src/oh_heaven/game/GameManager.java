ae46a00efe2d315dc01a4ae1adf81bea12b75b89
laviyre <54475683+laviyre@users.noreply.github.com>
2022-05-29 01:00:09 +1000

    Add trick class

--- src/oh_heaven/game/GameManager.java
+++ src/oh_heaven/game/GameManager.java
@@ -33,11 +33,12 @@ public class GameManager extends CardGame
     private int nbStartCards = 13;
     private int nbRounds = 3;
     private final Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
-    private boolean enforceRules=false;
 
     private Player[] players = new Player[nbPlayers];
 
     private final GraphicsManager graphics = new GraphicsManager();
+
+    private Trick trick;
 
     public void setSeed(int seed) {
         RandomHandler.getInstance().setRandom(seed, nbPlayers);
@@ -47,7 +48,8 @@ public class GameManager extends CardGame
     {
         nbRounds = PropertiesLoader.getNbRounds();
         nbStartCards = PropertiesLoader.getNbStartCards();
-        enforceRules = PropertiesLoader.getEnforceRules();
+        boolean enforceRules = PropertiesLoader.getEnforceRules();
+        trick = new Trick(enforceRules);
         setSeed(PropertiesLoader.getSeed());
     }
 
@@ -136,98 +138,51 @@ public class GameManager extends CardGame
             if (i instanceof Human)
                 ((Human) i).makeCardListener();
         }
-        //human.makeCardListener();
-        // graphics
+        
         RowLayout[] layouts = new RowLayout[nbPlayers];
         for (int i = 0; i < nbPlayers; i++) {
             layouts[i] = graphics.getLayout(this, players[i]);
         }
-//	    for (int i = 1; i < nbPlayers; i++) // This code can be used to visually hide the cards in a hand (make them face down)
-//	      hands[i].setVerso(true);			// You do not need to use or change this code.
-        // End graphics
     }
 
-    private void ruleViolationCheck(Suit lead, int nextPlayer) {
-        if (selected.getSuit() != lead && players[nextPlayer].getHand().getNumberOfCardsWithSuit(lead) > 0) {
-            // Rule violation
-            String violation = "Follow rule broken by player " + nextPlayer + " attempting to play " + selected;
-            System.out.println(violation);
-            if (enforceRules)
-                try {
-                    throw(new BrokeRuleException(violation));
-                } catch (BrokeRuleException e) {
-                    e.printStackTrace();
-                    System.out.println("A cheating player spoiled the game!");
-                    System.exit(0);
-                }
-        }
+    private void playRound()
+    {
+        // Select and display the trump suit
+        Actor trumpsActor = graphics.setTrumpGraphics(this, trick.newGame());
 
-    }
-
-    private void playRound() {
-        // Select and display trump suit
-        final Suit trumps = RandomHandler.getInstance().randomEnum(Suit.class);
-        Actor trumpsActor = graphics.setTrumpGraphics(this, trumps);
-        // End trump suit
-
-        Hand trick;
-        int winner;
-        Card winningCard;
-        Suit lead;
         int nextPlayer = RandomHandler.getInstance().getRandomPlayerNumber(); // randomly select player to lead for this round
         initBids(nextPlayer);
 
         for (int i = 0; i < nbPlayers; i++)
             graphics.updateScoreGraphics(this, players[i]);
 
-        for (int i = 0; i < nbStartCards; i++) {
-            trick = new Hand(deck);
-            selected = players[nextPlayer].lead(this,trumps);
-            // Lead with selected card
-            graphics.setTrickView(this, trick, selected);
-            // No restrictions on the card being lead
-            lead = (Suit) selected.getSuit();
-            players[nextPlayer].playCard(trick, selected);
-            winner = nextPlayer;
-            winningCard = selected;
-            // End Lead
+        for (int i = 0; i < nbStartCards; i++)
+        {
+            trick.newTrick(deck);
 
-            for (int j = 1; j < nbPlayers; j++) {
-                if (++nextPlayer >= nbPlayers) nextPlayer = 0;  // From last back to first
-                selected = players[nextPlayer].follow(this,trumps,trick);
+            for (int j = 0; j < nbPlayers; j++)
+            {
+                Player currentPlayer = players[nextPlayer];
+                Card cardPlayed = trick.playCard(this, currentPlayer);
+                graphics.setTrickView(this, trick.getTrickHand(), cardPlayed);
 
-                // Follow with selected card
-                graphics.setTrickView(this, trick, selected);
-                // Check: Following card must follow suit if possible
-                ruleViolationCheck(lead, nextPlayer);
-                // End Check
+                // Winner gets updated if it isn't the first card played
+                if (j != 0)
+                    trick.updateWinner(currentPlayer);
 
-                players[nextPlayer].playCard(trick, selected); // transfer to trick (includes graphic effect)
-                System.out.println("winning: " + winningCard);
-                System.out.println(" played: " + selected);
-                // System.out.println("winning: suit = " + winningCard.getSuit() + ", rank = " + (13 - winningCard.getRankId()));
-                // System.out.println(" played: suit = " +    selected.getSuit() + ", rank = " + (13 -    selected.getRankId()));
-                if (newWinnerCheck(selected, winningCard, trumps)) {
-                    System.out.println("NEW WINNER");
-                    winner = nextPlayer;
-                    winningCard = selected;
-                }
-                // End Follow
+                if (++nextPlayer == nbPlayers)
+                    nextPlayer = 0;
             }
-            delay(600);
-            graphics.hideTrick(this, trick);
-            nextPlayer = winner;
-            setStatusText("Player " + nextPlayer + " wins trick.");
-            players[nextPlayer].winTrick();
-            graphics.updateScoreGraphics(this, players[nextPlayer]);
-        }
-        removeActor(trumpsActor);
-    }
 
-    private boolean newWinnerCheck(Card selected, Card previous, Suit trumps) {
-        return (selected.getSuit() == previous.getSuit() && rankGreater(selected, previous)) ||
-                // trumped when non-trump was winning
-                (selected.getSuit() == trumps && previous.getSuit() != trumps);
+            delay(600);
+            graphics.hideTrick(this, trick.getTrickHand());
+            Player winner = trick.getWinner();
+            setStatusText("Player " + winner.getPlayerNumber() + " wins trick.");
+            winner.winTrick();
+            graphics.updateScoreGraphics(this, winner);
+        }
+
+        removeActor(trumpsActor);
     }
 
     public GameManager()
--- src/oh_heaven/game/Trick.java
+++ src/oh_heaven/game/Trick.java
@@ -2,21 +2,103 @@ package oh_heaven.game;
 
 import ch.aplu.jcardgame.*;
 import ch.aplu.jgamegrid.*;
+import oh_heaven.game.Player.*;
 
 public class Trick
 {
     private Hand trickHand;
+    private Suit currentSuit;
 
     private Suit trumpSuit;
     private Actor trumpActor;
 
-    public void newGame(CardGame game, )
+    private Player winner;
+    private Card winningCard;
+    private Card cardPlayed;
+
+    private boolean enforceRules;
+
+    public Trick(boolean enforceRules)
     {
+        this.enforceRules = enforceRules;
+    }
+
+    public Suit newGame()
+    {
+        trumpSuit = RandomHandler.getInstance().randomEnum(Suit.class);
+        return trumpSuit;
+    }
+
+    public void newTrick(Deck deck)
+    {
+        trickHand = new Hand(deck);
+        currentSuit = null;
+    }
+
+    public Card playCard(CardGame game, Player player)
+    {
+        boolean isFirstCard = currentSuit == null;
+
+        cardPlayed = isFirstCard ? player.lead(game, trumpSuit) : player.follow(game, trumpSuit, getTrickHand());
+
+        if (isFirstCard)
+        {
+            currentSuit = (Suit) cardPlayed.getSuit();
+            winningCard = cardPlayed;
+            winner = player;
+        }
+        else
+            ruleViolationCheck(player); // Check the player can play the card
+
+        player.playCard(trickHand, cardPlayed);
+
+        return cardPlayed;
+    }
+
+    private boolean newCardIsWinner() {
+        return (cardPlayed.getSuit() == winningCard.getSuit() && cardPlayed.getRankId() < winningCard.getRankId()) ||
+                // trumped when non-trump was winning
+                (cardPlayed.getSuit() == trumpSuit && cardPlayed.getSuit() != trumpSuit);
+    }
+
+    public void updateWinner(Player player)
+    {
+        // Print out current status of cards played
+        System.out.println("winning: " + winningCard);
+        System.out.println(" played: " + cardPlayed);
+
+        if (newCardIsWinner())
+        {
+            System.out.println("NEW WINNER");
+            winningCard = cardPlayed;
+            winner = player;
+        }
+    }
+
+    private void ruleViolationCheck(Player player) {
+        if (cardPlayed.getSuit() != currentSuit && player.getHand().getNumberOfCardsWithSuit(currentSuit) > 0) {
+            // Rule violation
+            String violation = "Follow rule broken by player " + player.getPlayerNumber() + " attempting to play " + cardPlayed;
+            System.out.println(violation);
+            if (enforceRules)
+                try {
+                    throw(new BrokeRuleException(violation));
+                } catch (BrokeRuleException e) {
+                    e.printStackTrace();
+                    System.out.println("A cheating player spoiled the game!");
+                    System.exit(0);
+                }
+        }
 
     }
 
-    public void newTrick()
+    public Hand getTrickHand()
     {
+        return trickHand;
+    }
 
+    public Player getWinner()
+    {
+        return winner;
     }
 }
